8c79c1e59f292aef7d9fb2be1eec2e0cf53307d8
Drew <drew.f.cummins@gmail.com>
2017-05-17 23:12:44 -0400

    tiny steps

--- BoltzmannPhysics/xcode/CharacterEngine/Characters/SimbiconCharacter.cpp
+++ BoltzmannPhysics/xcode/CharacterEngine/Characters/SimbiconCharacter.cpp
@@ -21,7 +21,7 @@ void SimbiconCharacter::setup(float height, vec3 pelvisX) {
     
     Body ground = RigidBody::create();
     ground->isGround = true;
-    hj = HingeJoint::create(pelvis, vec3(0,0,0), vec3(0,1,0), ground);
+    hj = HingeJoint::create(pelvis, vec3(0,0,0), vec3(0,0,1), ground);
     
     
     FState dummy1 = TemporalState::create(0.3f);
@@ -41,60 +41,51 @@ void SimbiconCharacter::setup(float height, vec3 pelvisX) {
 }
 
 vector<Constraint> SimbiconCharacter::getJoints() {
+//    return {lhip, rhip, lknee, rknee, back, backLat, lhipLatJoint, rhipLatJoint};
     return {lhip, rhip, lknee, rknee, back, backLat, lhipLatJoint, rhipLatJoint, hj};
 }
 
-//float SimbiconCharacter::getTargetInJointSpace(float thetaWorld, float thetaJoint, Body body, vec3 jointAxis, bool flip) {
-////    quat q(1,0,0,0);
-//    quat q = glm::rotate(quat(), thetaWorld, jointAxis);
-//    vec3 x = orthogonal(jointAxis);
-//    vec3 xInWorldSpace = q * x;
-//    vec3 xInJointSpace = inverse(body->R) * xInWorldSpace;
-////    if (dot(xInJointSpace, jointAxis) > 0) {
-////        flip = true;
-////    } else {
-////        flip = false;
-////    }
-//    vector<vec2> projection = Utils::projectPointsOntoPlane({xInJointSpace, x}, vec3(), jointAxis);
-//    vec2 u1 = normalize(projection[0]);
-//    vec2 u2 = normalize(projection[1]);
-//    float dtheta = glm::acos(dot(u1, u2));
-////    if (fabs(dtheta) > glm::pi<float>()*0.5) {
-////        dtheta -= glm::pi<float>();
-////    }
-//    if (Utils::isClockwise({u1, u2, vec2()})) {
-//        dtheta = -dtheta;
-//    }
-//    
-//    cout << dtheta << endl;
-////    if (flip) {
-////        dtheta = -dtheta;
-////    }
-//    return thetaJoint + dtheta;
-//}
-
 float SimbiconCharacter::getTargetInJointSpace(float thetaWorld, float thetaJoint, Body body, vec3 jointAxis, bool flip) {
-    vec3 x = Utils::orthogonal(jointAxis);
-    vec3 targetInWorldSpace = glm::rotate(x, thetaWorld, jointAxis);
-    vec3 currentInJointSpace = glm::rotate(x, thetaJoint, jointAxis);
-    vec3 currentInWorldSpace = body->R * currentInJointSpace;
-    cout << targetInWorldSpace << " - " << currentInWorldSpace << endl;
-//    vec3 xInJointSpace = body->R * glm::rotate(x, thetaWorld, jointAxis);
-//    vec3 xInWorldSpace = body->R * x;
-    vector<vec2> projection = Utils::projectPointsOntoPlane({targetInWorldSpace, currentInWorldSpace}, vec3(12,2,10), jointAxis);
-    vec2 u1 = normalize(projection[0]);
-    vec2 u2 = normalize(projection[1]);
+    vec3 up = vec3(0,1,0); // orthogonal(jointAxis);
+    if (dot(jointAxis, vec3(0,0,1)) > 0.1) {
+        up = vec3(1,0,0);
+    }
+    vec3 upInJointSpace = inverse(body->R) * up;
     
-    cout << u1 << " - " << u2 << " = " << dot(u1, u2) << endl;
-    float dtheta = glm::acos(Utils::clamp(dot(u1, u2), 0.f, 1.f));
+    vector<vec2> plane = Utils::projectPointsOntoPlane({upInJointSpace, up}, vec3(), jointAxis);
+    vec2 u1 = normalize(plane[0]);
+    vec2 u2 = normalize(plane[1]);
+    
+    float dtheta = glm::acos(dot(u1, u2));
     if (Utils::isClockwise({u1, u2, vec2()})) {
-//        cout << "OKAY" << endl;
         dtheta = -dtheta;
     }
-    cout << "DTHETA: " << dtheta << endl;
-//    cout << dtheta << endl;
+    
     return thetaJoint + dtheta;
 }
+
+//float SimbiconCharacter::getTargetInJointSpace(float thetaWorld, float thetaJoint, Body body, vec3 jointAxis, bool flip) {
+//    vec3 x = Utils::orthogonal(jointAxis);
+//    vec3 targetInWorldSpace = glm::rotate(x, thetaWorld, jointAxis);
+//    vec3 currentInJointSpace = glm::rotate(x, thetaJoint, jointAxis);
+//    vec3 currentInWorldSpace = body->R * currentInJointSpace;
+//    cout << targetInWorldSpace << " - " << currentInWorldSpace << endl;
+////    vec3 xInJointSpace = body->R * glm::rotate(x, thetaWorld, jointAxis);
+////    vec3 xInWorldSpace = body->R * x;
+//    vector<vec2> projection = Utils::projectPointsOntoPlane({targetInWorldSpace, currentInWorldSpace}, vec3(12,2,10), jointAxis);
+//    vec2 u1 = normalize(projection[0]);
+//    vec2 u2 = normalize(projection[1]);
+//    
+//    cout << u1 << " - " << u2 << " = " << dot(u1, u2) << endl;
+//    float dtheta = glm::acos(Utils::clamp(dot(u1, u2), 0.f, 1.f));
+//    if (Utils::isClockwise({u1, u2, vec2()})) {
+////        cout << "OKAY" << endl;
+//        dtheta = -dtheta;
+//    }
+//    cout << "DTHETA: " << dtheta << endl;
+////    cout << dtheta << endl;
+//    return thetaJoint + dtheta;
+//}
 
 
 
@@ -148,49 +139,22 @@ void SimbiconCharacter::update(float dt) {
     const float cd = 0.5f;
     const float cv = 0.2f;
     
+    Theta[swing] = getTargetInJointSpace(Theta[swing], swingHip->cacheTheta(), swingHip->b2, swingHip->a2);
+    cout << state->Theta[swing] << ", " << Theta[swing] << endl;
+    Theta[swing+1] = getTargetInJointSpace(Theta[swing+1], -swingHipLat->cacheTheta(), swingHipLat->b1, swingHipLat->a1);
     
     Theta[swing]    += cd * dSagittal + cv * vSagittal;
     Theta[swing+1]  += cd * dCoronal  + cv * vCoronal;
     
-    vec3 up = vec3(0,1,0);
-    vec3 upInTorsoSpace = inverse(torso->R) * up;
-    
-    // up in the points to be transformed is actually just the torso up vector
-    vector<vec2> torsoPlane = Utils::projectPointsOntoPlane({upInTorsoSpace, up}, back->r1, back->a1);
-    vec2 u1 = normalize(torsoPlane[0]);
-    vec2 u2 = normalize(torsoPlane[1]);
-    
-    float dthetaTorso = glm::acos(dot(u1, u2));
-    if (Utils::isClockwise({u1, u2, vec2()})) {
-        dthetaTorso = -dthetaTorso;
-    }
-    
-    vec3 upInTorsoLatSpace = torsoLat->R * up;
-    
-    torsoPlane = Utils::projectPointsOntoPlane({upInTorsoLatSpace, up}, backLat->r1, backLat->a1);
-    u1 = normalize(torsoPlane[0]);
-    u2 = normalize(torsoPlane[1]);
-    
-    float dthetaTorsoLat = acos(dot(u1, u2));
-    if (Utils::isClockwise({u1, u2, vec2()})) {
-        dthetaTorsoLat = -dthetaTorsoLat;
-    }
-    
-//    // set torso orientation to up in world coordinates:
-//    Theta[BACK] = back->cacheTheta() + dthetaTorso;
-//    Theta[BACKLAT] = backLat->cacheTheta() + dthetaTorsoLat;
-    
-    cout << "BEFORE: " << Theta[BACK] << ", " << back->cacheTheta() << ", " << endl;;
     Theta[BACK] = getTargetInJointSpace(Theta[BACK], back->cacheTheta(), torso, back->a1);
-    cout << "AFTER: " << Theta[BACK] << endl << endl;
-//    Theta[BACKLAT] = getTargetInJointSpace(Theta[BACKLAT], backLat->cacheTheta(), torsoLat, backLat->a1);
+    Theta[BACKLAT] = getTargetInJointSpace(Theta[BACKLAT], backLat->cacheTheta(), torsoLat, backLat->a1);
     
 //    cout << (back->cacheTheta() + dthetaTorso) << " - " << Theta[BACK] << endl;
 //////
 //////    // find stance hip torque:
 //////    // -back - swing hip for coronal and sagittal
-    Theta[stance] += -dthetaTorso - (Theta[swing] - swingTheta);
-    Theta[stance+1] += -dthetaTorsoLat - (Theta[swing+1] - swingLatTheta);
+//    Theta[stance] += -(Theta[BACK] - state->Theta[BACK]) - (Theta[swing] - swingTheta);
+//    Theta[stance+1] += -(Theta[BACKLAT] - state->Theta[BACKLAT]) - (Theta[swing+1] - swingLatTheta);
     
     for (int i = 0; i < muscles.size(); i++) {
         muscles[i]->mapThetaToT(Theta[i]);
